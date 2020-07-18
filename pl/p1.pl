16f19f39f8988c0d28564f7d5a65dd1517f4c99d
Greenbirch2007 <291109028@qq.com>
2020-07-18 22:58:40 +0800

    7.18 chg

--- pl/p1.pl
+++ pl/p1.pl
@@ -1,7 +1,14 @@
 
-
-@fred = 6..10;
-@barney = reverse(@fred);
-foreach (@barney){
-	print "$_\n";
-	}
+use 5.010;
+running_sum(5,6);
+running_sum(1..3);
+running_sum(4);
+sub running_sum{
+	state $sum = 0;
+	state @numbers;
+	foreach my $number (@_){
+		push @numbers,$nubmer;
+		$sum += $number;
+		}
+		say "The sum of (@numbers) is $sum";
+		}
