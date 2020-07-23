85b12b559942ff46bfd5ad6b1c13f86fd6a99408
Greenbirch2007 <291109028@qq.com>
2020-07-23 23:47:11 +0800

    7.23 chg

--- pl/p1.pl
+++ pl/p1.pl
@@ -1,13 +1,6 @@
 
-use CGI qw(:all);
 
-print header(),
-	start_html("this is the page title"),
-	h1("Input parameters");
-	
-my $list_items;
-foreach my $param (param()){
-	$list_items .= li("$param:".param($param));
-	}
-print ul($list_items);
-print end_html();
+
+use List::Util qw(first);
+
+my $first_match = first{/\bPebbles\b/} @characters;
