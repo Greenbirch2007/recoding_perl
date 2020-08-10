a5ae3ecbd05e6dca6828f28c3f613cd456570d0b
Greenbirch2007 <291109028@qq.com>
2020-08-10 23:02:05 +0800

    8.10 chg

--- pl/p1.pl
+++ pl/p1.pl
@@ -1,21 +1,11 @@
 
-my %hash =(
-    'John' => '1982.1.5',
-   'Paul' => '1978.11.3',
-    'Lee' => '1976.3.2',
-    'Mary' => '1980.6.23',
+print "&times(3)"；
 
-);
-
-
-my @bir;
-while (my($key,$value) = each (%hash)) {
-    if ($value =~/^198/){
-        push @bir,$key;
+sub times{
+    my $input = shift;
+    my $total =1;
+    for (my $i; $i<=$input; $i++) {
+        $total *= $i;
     }
-}
-
-print @bir;
-for (@bir){
-    print "$_,$hash{$_}\n";
+    
 }
