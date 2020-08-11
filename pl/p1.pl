8bf35bf6f6651f4072300dbcbe1335141bab89a6
Greenbirch2007 <291109028@qq.com>
2020-08-11 18:17:33 +0800

    8.11 chg

--- pl/p1.pl
+++ pl/p1.pl
@@ -1,11 +1,8 @@
-
-print "&times(3)"；
-
-sub times{
-    my $input = shift;
-    my $total =1;
-    for (my $i; $i<=$input; $i++) {
-        $total *= $i;
-    }
-    
+#!/usr/bin/perl
+use strict;
+chomp(my $input = <STDIN>);
+if ($input =~ /['"](.+?)\1/) {
+print "$1\n";
+} else {
+print "比对失败\n";
 }
