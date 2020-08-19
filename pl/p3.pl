b861a6047bcefe595ec19f848f98ef12438e6e1a
Greenbirch2007 <291109028@qq.com>
2020-08-19 13:29:42 +0800

    8.19 chg

--- pl/p3.pl
+++ pl/p3.pl
@@ -1,11 +1,22 @@
 #!/usr/bin/perl
 
 
-use strict;
-chomp(my $input = <STDIN>);
-($input > 60)? print "pass":print "failed";
-# if ($input < 60) {
-# print "不及格";
-# } else {
-# print "及格";
+# use strict;
+
+# my %tel = ("Peter", 27216543, "Ruby", 27820022);
+
+
+# open FILE,">> tt.txt";
+# for (keys %tel){
+#     print FILE "$_ => $tel{$_}\n";
+
 # }
+# close FILE;
+
+use strict;
+
+open FILE,"tt.txt" or  die $!;
+while (<FILE>) {
+    print $_;
+}
+close FILE;
