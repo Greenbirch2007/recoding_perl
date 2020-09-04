23136e9c7c87f7a2604371682bf755426550a052
Greenbirch2007 <291109028@qq.com>
2020-09-04 20:36:20 +0800

    就收集脚本吧,放弃wins

--- pl/p1.pl
+++ pl/p1.pl
@@ -1,37 +1,5 @@
 
-#3. 利用散列%hash = (john, 24, mary, 28, david, 22, paul, 28)进行排序
-#散列的值排序，如果两个元素的值相等，则依照键值进行字符串排序。
-# 说的是两个元素，就是值是想等
-# my %hash =(john, 24, mary, 28, david, 22, paul, 28);
+my @aray = qw/John Paul May/;   # 一个数组
+my $array_ref = \@array;   # 取得这个数组的引用
 
-
-#使用便利？
-
-my %hash =(john, 24, mary,28, david, 22, paul, 28,aaa,28,bb,28,cc,28);
-my @order = sort ffo keys %hash;
-print "@order \n";   # 依序是 david john mary
-
-
-
-
-# foreach my $key ( sort { $hash{$a} <=> $hash{$b} } keys %hash ) {
-#     if ($hash{$a}  == $hash{$b} ){
-#        my @farr sort{ $a cmp $b }keys %hash ;
-#     }
-#     print "@farr \n";
-
-
-#     #do something with ($key, $value)
-
-# }
-
-# 按哈希键bai的数值大小排du序：
-# my @key =sort {$a <=> $b} keys %hash;
-# @key里头存的是按哈zhi希键的dao数内值大小排序后容的键。
-# 按哈希值的数值大小排序：
-# my @key =sort {$hash{$a} <=> $hash{$b}} keys %hash;
-# @key里头存的是按哈希值的数值大小排序后的键
-
-sub ffo {
-    $hash{$a} <=> $hash{$b} or $a cmp $b;
-}
+print @{$array_ref};
--- pl/p2.pl
+++ pl/p2.pl
@@ -1,5 +1,18 @@
-
-
+#!/usr/bin/perl -w
 use strict;
-chomp(my $input= <STDIN>);
-print "hello $input \n";
+use DBI;
+my $dbh = DBI->connect('dbi:mysql:database=perlbook', 'root', '123456');
+my $create = <<"END";
+CREATE TABLE address (
+name varchar(24),
+cellphone varchar(12),
+company vrchar(24),
+title varchar(12)
+);
+END
+$dbh->do($create) or die "can't create"; # 先把资料表格建起来
+my $sql;
+$sql = "INSERT INTO address VALUES ('王小明', '0911111111', '甲上信息', '项目经理')";
+$dbh->do($sql);
+$sql = "INSERT INTO address VALUES ('李小华', '0922222222', '乙下软件', '业务经理')";
+$dbh->do($sql);
